a58b99615ee728b407973f5f47819c16544c5d04
AljenNicole <45374723+AljenNicole@users.noreply.github.com>
2018-11-27 11:07:39 +0800

    Create Activity_A_Lee.py

---
+++ Activity_A_Lee.py
@@ -0,0 +1,25 @@
+#One
+print("One")
+for x in range (1,17,3):
+
+    print(x, end='\t')
+print()
+print()
+#Two
+print("Two")
+for x in range (100,75,-7):
+
+    print(x,end='\t')
+print()
+print()
+#Three
+
+print("Three")
+for x in range(1,5):
+
+    if x % 3 == 0:
+       print ("1", end = '\t')
+    else:
+       print("0", end = '\t')
+print()
+print()
