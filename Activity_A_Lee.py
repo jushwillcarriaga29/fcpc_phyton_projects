782caeeb1802a89d6b9328a3997f03ecf811e905
jushwillcarriaga29 <45374703+jushwillcarriaga29@users.noreply.github.com>
2018-11-27 11:13:56 +0800

    Update Activity_A_Lee.py

--- Activity_A_Lee.py
+++ Activity_A_Lee.py
@@ -1,20 +1,20 @@
-#One
-print("One")
+#first
+print("1st")
 for x in range (1,17,3):
 
     print(x, end='\t')
 print()
 print()
-#Two
-print("Two")
+#second
+print("2nd")
 for x in range (100,75,-7):
 
     print(x,end='\t')
 print()
 print()
-#Three
+#Third
 
-print("Three")
+print("3rd")
 for x in range(1,5):
 
     if x % 3 == 0:
